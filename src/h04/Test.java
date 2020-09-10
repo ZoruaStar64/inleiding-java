9c1c15b5c0faf82b1ce15f94fe07e47adb371491
ZoruaStar64 <sanderb119@gmail.com>
2020-09-10 13:43:37 +0200

    H6 af

--- src/h04/Test.java
+++ src/h04/Test.java
@@ -10,6 +10,6 @@ public class Test extends Applet {
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setStroke(new BasicStroke(3));
-        g2.drawLine(1,10,100,10);   //thick
+
     }
 }
---
+++ src/h06/test/test2.java
@@ -0,0 +1,24 @@
+package h06.test;
+
+//Voorbeeld 6.4
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class test2 extends Applet {
+    int a, b, c;
+    double uitkomst;
+
+
+    public void init() {
+        a = 8;
+        b = 8;
+        c = 3;
+        uitkomst = a + b / c;
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("De uitkomst is: " + uitkomst, 20, 20);
+    }
+}
