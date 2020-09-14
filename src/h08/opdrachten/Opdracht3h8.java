fd89610d99a4acdc775a89584c4c3c9ae00f7a88
ZoruaStar64 <sanderb119@gmail.com>
2020-09-14 11:05:38 +0200

    H8 opdracht 3 af (met hulp van Jorn)

--- src/h08/opdrachten/Opdracht3h8.java
+++ src/h08/opdrachten/Opdracht3h8.java
@@ -1,4 +1,43 @@
 package h08.opdrachten;
 
-public class Opdracht3h8 {
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class Opdracht3h8 extends Applet {
+    TextField tekstvak;
+    Button ok;
+    String s;
+    //int Procent;
+    double teller;
+    double a;
+
+
+    public void init() {
+        tekstvak = new TextField("", 30);
+        ok = new Button("Ok");
+        ok.addActionListener(new KnopListener());
+        add(tekstvak);
+        add(ok);
+        s = "";
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawString("Typ een getal in en klik op Ok om uw totaalbedrag inclusief btw te zien.", 10, 50);
+        g.drawString("€ " + s, 100, 100);
+        g.drawString("Uw totaal inclusief btw komt hier onder te staan.", 80, 85);
+    }
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            teller= 1.21;
+            s = tekstvak.getText();
+            a = Double.parseDouble( s );
+            s = "" + a * teller;
+            tekstvak.setText("");
+            repaint();
+        }
+    }
 }
