2f1f23ad4cc79c9e9f7db36ad372924a3826dd2f
ZoruaStar64 <sanderb119@gmail.com>
2020-09-18 14:54:58 +0200

    Praktijkopdracht H11 half af

--- src/h11/Opdrachten/PraktijkopdrachtH11.java
+++ src/h11/Opdrachten/PraktijkopdrachtH11.java
@@ -3,34 +3,47 @@ package h11.Opdrachten;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PraktijkopdrachtH11 extends Applet {
     TextField tekstvak;
     Label label;
     Button button;
+    int teller2;
+    int teller;
+    int y = 50;
+    String ingave1;
+    int ingave2;
 
     public void init() {
         tekstvak = new TextField("", 5);
-        label = new Label("Cijfer 1");
-        //tekstvak.addActionListener(new TekstvakListener());
+        label = new Label("Voer een getal in en klik op Ok");
+        tekstvak.addActionListener(new TekstvakListener());
         button = new Button("Ok");
+        button.addActionListener(new TekstvakListener());
         add(tekstvak);
         add(label);
         add(button);
+
     }
 
     public void paint(Graphics g) {
-        int teller2;
-        int teller;
-        int tafel = 1;
-        int y = 0;
+                for (teller = 1; teller <= 10; teller++) {
 
-
-        for (teller = 1; teller <= 10; teller++) {
-            y += 20;
-            g.setFont(new Font("TimesRoman",Font.BOLD,20));
-            teller2 = tafel * teller;
-            g.drawString("" + teller + " x " + tafel + " = " + teller2, 50, y);
+                    g.setFont(new Font("TimesRoman", Font.BOLD, 20));
+                    ingave2 = Integer.parseInt(ingave1);
+                    teller2 = ingave2 * teller;
+                    g.drawString(teller + " x " + ingave2 + " = " + teller2, 100, y);
+                    y += 20;
+                }
+            }
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            ingave1 = tekstvak.getText();
+            tekstvak.setText("");
+            y = 50;
+            repaint();
         }
     }
 }
