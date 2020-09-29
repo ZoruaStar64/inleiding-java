3c2aeca59caf3e712b21a9397409a76d598a506a
ZoruaStar64 <sanderb119@gmail.com>
2020-09-29 10:02:37 +0200

    H13 opdracht 2 bijna klaar

--- src/h13/h13Opdrachten/opdracht2h13.java
+++ src/h13/h13Opdrachten/opdracht2h13.java
@@ -1,4 +1,36 @@
 package h13.h13Opdrachten;
 
-public class opdracht2h13 {
+
+import java.awt.*;
+import java.applet.*;
+
+public class opdracht2h13 extends Applet{
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g){
+      int x1 = 50;
+      int y1 = 100;
+      int width = 30;
+      int height = 20;
+
+    aRedGreatWall(g,x1,y1,width,height);
+
+    }
+
+    private void aRedGreatWall(Graphics g, int x1, int y1, int width, int height) {
+        for (int loop = 0; loop < 4; loop++) {
+            x1 = 50;
+            for (int row = 0; row < 8; row++) {
+                g.setColor(Color.red);
+                g.fillRect(x1, y1, width, height);
+                g.setColor(Color.black);
+                g.drawRect(x1, y1, width, height);
+                y1 = (height * loop + 120);
+                x1 += width;
+            }
+        }
+    }
 }
