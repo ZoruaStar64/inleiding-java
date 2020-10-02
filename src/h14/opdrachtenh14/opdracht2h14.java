d5a9547e42c8072d0a7cddd16a2bb6c11a8ef1b1
ZoruaStar64 <sanderb119@gmail.com>
2020-10-02 09:42:40 +0200

    H14 opdracht 2 speler 1-4 wordt geshowed met 10 kaarten moet alleen nog voor zorgen dat ze verschillende kaarten krijgen en geen dubbele kaarten kunnen hebben in het spel

--- src/h14/opdrachtenh14/opdracht2h14.java
+++ src/h14/opdrachtenh14/opdracht2h14.java
@@ -8,8 +8,9 @@ import java.awt.event.ActionListener;
 // doel: zorg ervoor dat je een applet maakt die 10 speelkaarten deelt (52 + 3 jokers)
 // daarna doe je dat voor 3 andere personen nogmaals
 // dit zou mogelijk kunnen zijn met twee for loops
+// Zorg ervoor dat er geen dubbele kaarten in het spel zitten bvb: speler 1 en 3 hebben allebei Clover of 4 dat kan niet
 public class opdracht2h14 extends Applet {
-    String card;
+    String[] card;
     Button button;
     Label label;
     int theChosenOne = 0;
@@ -20,8 +21,10 @@ public class opdracht2h14 extends Applet {
     String[] Jokers = {"Joker #1", "Joker #2", "Joker #3"};
     int onetothirteen = 0;
     int onetothree = 0;
+    int y = 15;
 
     public void init() {
+        card = new String[10];
         button = new Button("Generate card");
         button.addActionListener(new CardListener());
         add(button);
@@ -30,44 +33,43 @@ public class opdracht2h14 extends Applet {
     }
 
     public void paint(Graphics g) {
-        resize(600, 600);
-        g.drawString("Your card is " + card, 150, 150);
+        resize(1000, 600);
+        for (int aantal = 0; aantal <= 10; aantal++) {
+            g.drawString("Player one's card are " + card[aantal], 10, (y * aantal + 150));
+            g.drawString("Player two's card are " + card[aantal], 260, (y * aantal + 150));
+            g.drawString("Player three's card are " + card[aantal], 500, (y * aantal + 150));
+            g.drawString("Player four's card are " + card[aantal], 740, (y * aantal + 150));
+        }
     }
 
     public class CardListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            theChosenOne = (int) (Math.random() * 5 + 1);
+            for (int aantal = 0; aantal <= 10; aantal++) {
+                theChosenOne = (int) (Math.random() * 5 + 1);
 
-            if (theChosenOne == 5) {
-                onetothree = (int) (Math.random() * 3);
-                System.out.println(onetothree);
-            }
+                if (theChosenOne == 5) {
+                    onetothree = (int) (Math.random() * 3);
+                    System.out.println(onetothree);
+                }
 
-            if ( theChosenOne < 5) {
-                onetothirteen = (int) (Math.random() * 13);
-                System.out.println(onetothirteen);
-            }
+                if (theChosenOne < 5) {
+                    onetothirteen = (int) (Math.random() * 13);
+                    System.out.println(onetothirteen);
+                }
 
-            if (theChosenOne == 1) {
-                card = "Spade " + Spade[onetothirteen];
+                if (theChosenOne == 1) {
+                    card[aantal] = "Spade " + Spade[onetothirteen];
+                } else if (theChosenOne == 2) {
+                    card[aantal] = "Heart " + Heart[onetothirteen];
+                } else if (theChosenOne == 3) {
+                    card[aantal] = "Diamond " + Diamond[onetothirteen];
+                } else if (theChosenOne == 4) {
+                    card[aantal] = "Clover " + Clover[onetothirteen];
+                } else if (theChosenOne == 5) {
+                    card[aantal] = "" + Jokers[onetothree];
+                }
+                repaint();
             }
-
-            else if (theChosenOne == 2) {
-                card = "Heart " + Heart[onetothirteen];
-            }
-
-            else if (theChosenOne == 3) {
-                card = "Diamond " + Diamond[onetothirteen];
-            }
-
-            else if (theChosenOne == 4) {
-                card = "Clover " + Clover[onetothirteen];
-            }
-
-            else if (theChosenOne == 5) {
-                card = "" + Jokers[onetothree];
-            }
-            repaint();
         }
     }
 }
