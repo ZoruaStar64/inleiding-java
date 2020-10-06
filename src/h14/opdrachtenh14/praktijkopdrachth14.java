3f18380bc28f167848ed7dc920b1921aced3ad4e
ZoruaStar64 <sanderb119@gmail.com>
2020-10-06 11:28:53 +0200

    Basis van H14 praktijkopdracht af nu de bot slimmer maken en plaatjes d'r bij

--- src/h14/opdrachtenh14/praktijkopdrachth14.java
+++ src/h14/opdrachtenh14/praktijkopdrachth14.java
@@ -5,6 +5,8 @@ import java.applet.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Random;
 
 public class praktijkopdrachth14 extends Applet {
 
@@ -18,6 +20,13 @@ public class praktijkopdrachth14 extends Applet {
     TextField tekstvak;
     Label label;
     int aantal = 23;
+    String grabber;
+    String Errortext;
+    String humantext;
+    String computertext;
+    String[] Botchoices;
+    int converter;
+    boolean PlayerTurn;
 
     public void init() {
 
@@ -29,22 +38,27 @@ public class praktijkopdrachth14 extends Applet {
         Win = getImage(pad, "Wario-loses.jpg");
         button = new Button("Take");
         tekstvak = new TextField();
-        label = new Label("Subtract 1 to 3 coins");
+        label = new Label("Take 1, 2 or 3 coins");
         button.addActionListener(new Subtractionlistener());
         tekstvak.addActionListener(new Subtractionlistener());
         add(button);
         add(tekstvak);
         add(label);
+        Botchoices = new String[3];
 
     }
     public void paint(Graphics g) {
         //LoseSound.play();
-        resize(400,300);
+        resize(400,400);
+        g.drawString("" + Errortext, 105,50);
+        g.drawString("" + humantext, 105,50);
+        g.drawString("" + computertext, 105, 65);
+        g.drawString("The total of remaining coins = " + aantal, 105, 80);
         int j = 1;
         int k = 1;
         for ( int i = 1; i <= aantal; i++) {
             k++;
-            g.drawImage(coin, (40 * k + 20), (50 * j ), 40, 50, this);
+            g.drawImage(coin, (40 * k + 20), (50 * j + 50 ), 40, 50, this);
             if (i % 5 == 0) {
                 j++;
                 k = 1;
@@ -55,9 +69,50 @@ public class praktijkopdrachth14 extends Applet {
 
     public class Subtractionlistener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-        /*if (coin = 0) {
+            // De menselijke speler's input
+            grabber = tekstvak.getText();
+            converter = Integer.parseInt(grabber);
+
+                PlayerTurn = true;
+                if (converter == 1) {
+                    aantal = aantal - converter;
+                    humantext = "You took 1 coin";
+                    Errortext = "";
+                }
+           else if (converter == 2) {
+                aantal = aantal - converter;
+                    humantext = "You took 2 coin";
+                    Errortext = "";
+            }
+           else if (converter == 3) {
+                aantal = aantal - converter;
+                    humantext = "You took 3 coin";
+                    Errortext = "";
+            }
+            else {
+                Errortext = "Use the numbers 1, 2 or 3";
+                humantext = "";
+            }
+            System.out.println(aantal);
+            // Wario's beurt
+            PlayerTurn = false;
+            int random = new Random().nextInt(Botchoices.length);
+            if (!PlayerTurn) {
+                aantal = aantal - (random + 1);
+                computertext = "Wario took " + "" + random + " coin(s)";
+                Errortext = "";
+            }
+            PlayerTurn = true;
+            if (aantal < 0) {
+                aantal = 0;
+            }
+            repaint();
+        if (aantal == 0 && !PlayerTurn) {
             LoseSound.play();
-            }*/
+            }
+        else if (aantal == 0 && PlayerTurn) {
+            WinSound.play();
+        }
         }
     }
 }
